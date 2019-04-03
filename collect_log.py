7206df91b347e122332cde6b68ec14a86125e889
Bright Lee <bright.th.lee@fii-foxconn.com>
2019-04-03 17:23:56 +0800

    1. Refine the coding structure 2. Only compute the CPK values with lower/upper bound

--- collect_log.py
+++ collect_log.py
@@ -10,10 +10,10 @@ import datetime
 import xlsxwriter
 import zipfile
 import operator
+import enum
 from StringIO import StringIO
 from xlsxwriter.utility import xl_range,xl_col_to_name
 
-# argv[1]: file
 if len(sys.argv) == 3:
     path = sys.argv[1]
     output = sys.argv[2]
@@ -39,31 +39,47 @@ format_pink = workbook.add_format({'bg_color': '#FF8AD8'})
 format_red = workbook.add_format({'bg_color': '#FF0000', 'font_color': '#FFFFFF'})
 
 worksheet_all = workbook.add_worksheet('all_data')
-worksheet_all.freeze_panes(11,1)
 
-worksheet_all.write_string(0,0,'Filename')
-worksheet_all.write_string(0,1,'DUT_ID')
-worksheet_all.write_string(0,2,'START_DATE_TIME')
-worksheet_all.write_string(0,3,'STATUS')
-worksheet_all.write_string(0,4,'FAILURE_CODE')
+ITEMs = ['Filename', 'DUT_ID', 'START_DATE_TIME', 'STATUS', 'FAILURE_CODE']
+LSLs = ['LSL','','','','']
+USLs = ['USL','','','','']
+all_data_rows = []
 
-worksheet_all.write_string(1,0,'LSL')
-worksheet_all.write_string(2,0,'USL')
-worksheet_all.write_string(3,0,'MIN')
-worksheet_all.write_string(4,0,'MAX')
-worksheet_all.write_string(5,0,'CPK')
-worksheet_all.write_string(6,0,'CPK-')
-worksheet_all.write_string(7,0,'CPK+')
-worksheet_all.write_string(8,0,'STDEV')
-worksheet_all.write_string(9,0,'AVERAGE')
-worksheet_all.write_string(10,0,'Useful COUNT')
-
-all_row_num = 11
 column_num = 0
 cpk_columns = []
-csv_items = []
-csv_lsl = []
-csv_usl = []
+
+class scz_columns(enum.IntEnum):
+    TEST_ITEMS = 0
+    DUT_ID = 0
+    NUMERIC_VALUE = 5
+    NUMERIC_MAX= 6
+    NUMERIC_MIN= 7
+    TEXT_VALUE = 9
+    STATUS = 13
+    FAILURE_CODE = 14
+    START_DATE_TIME = 16
+
+def set_conditional_format(worksheet, row, column):
+    worksheet.conditional_format(row,column,row,column,{'type': 'cell',
+                                             'criteria': '>=',
+                                             'value': 2,
+                                             'format': format_green})
+    worksheet.conditional_format(row,column,row,column,{'type': 'cell',
+                                             'criteria': 'between',
+                                             'minimum': 1,
+                                             'maximum': 1.33,
+                                             'format': format_yellow})
+    worksheet.conditional_format(row,column,row,column,{'type': 'cell',
+                                             'criteria': 'between',
+                                             'minimum': 0.67,
+                                             'maximum': 1,
+                                             'format': format_pink})
+    worksheet.conditional_format(row,column,row,column,{'type': 'cell',
+                                             'criteria': '<',
+                                             'value':0.67,
+                                             'format': format_red})
+
+globals().update(scz_columns.__members__)
 
 if '.zip' in files[0]:
     for z in files:
@@ -81,32 +97,20 @@ if '.zip' in files[0]:
         measurecsv = csv.reader(measurefile,delimiter=',')
 
         next(measurecsv) # bypass title
-        column_num = 5
         for row_i in measurecsv:
-            worksheet_all.write_string(0,column_num,row_i[0]) # Test item name
-            ok_to_cpk = False
-            if row_i[7] not in (None, "", ' ', '--'): 
-                worksheet_all.write_number(1,column_num,float(row_i[7])) # Number Min
-                ok_to_cpk = True
-            if row_i[6] not in (None, "", ' ', '--'): 
-                worksheet_all.write_number(2,column_num,float(row_i[6])) # Number Max
-                ok_to_cpk = True
-            if row_i[5] not in (None, "", ' ', '--'): # Numeric Value
-                try:
-                    float(row_i[5]) # check if could convert to numbers
-                    if ok_to_cpk == True:
-                        cpk_columns.append(column_num)
-                        csv_items.append(row_i[0])
-                        csv_lsl.append(row_i[7])
-                        csv_usl.append(row_i[6])
-                except:
-                    None
+            ITEMs.append (row_i[TEST_ITEMS])
+            if row_i[NUMERIC_MIN] not in (None, "", ' ', '--'): 
+                LSLs.append(row_i[NUMERIC_MIN])
+            else:
+                LSLs.append('')
+            if row_i[NUMERIC_MAX] not in (None, "", ' ', '--'): 
+                USLs.append(row_i[NUMERIC_MAX])
+            else:
+                USLs.append('')
 
-            column_num += 1
         archive.close()
         break
 
-    all_csv_row = []
 
     for z in files:
         archive = zipfile.ZipFile(z,'r')
@@ -124,37 +128,25 @@ if '.zip' in files[0]:
             continue
 
         metacsv = csv.reader(metafile,delimiter=',')
-        row = []
+        meta_row = []
 
         next(metacsv) # bypass title
-        row = next(metacsv) 
-        worksheet_all.write_string(all_row_num, 0, f) # filename / folder
-        worksheet_all.write_string(all_row_num, 1, row[0]) # DUT_ID
-        worksheet_all.write_string(all_row_num, 2, row[16]) # START_DATE_TIME
-        worksheet_all.write_string(all_row_num, 3, row[13]) # STATUS
-        worksheet_all.write_string(all_row_num, 4, row[14]) # FAILURE_CODE
-
-        csv_row = [row[16], row[0]] # START_DATE_TIME, DUT_ID
+        meta_row = next(metacsv) 
+        mea_row = [f, meta_row[DUT_ID], meta_row[START_DATE_TIME], meta_row[STATUS], meta_row[FAILURE_CODE]]
 
         measurecsv = csv.reader(measurefile,delimiter=',')
         next(measurecsv) # bypass title
-        column_num = 5
         for row_i in measurecsv:
-            if row_i[5] in (None, "", ' '):
-                worksheet_all.write_string(all_row_num, column_num, row_i[9])
+            if row_i[NUMERIC_VALUE] in (None, "", ' '):
+                mea_row.append(row_i[TEXT_VALUE])
             else:
                 try:
-                    worksheet_all.write_number(all_row_num, column_num, float(row_i[5]))
-                    if row_i[0] in csv_items:
-                        csv_row.append(row_i[5])
+                    mea_row.append(float(row_i[NUMERIC_VALUE]))
                 except:
-                    worksheet_all.write_string(all_row_num, column_num, row_i[5])
-            column_num += 1
+                    mea_row.append(float([NUMERIC_VALUE]))
 
-        all_row_num += 1
+        all_data_rows.append(mea_row)
         archive.close()
-        if row[13] == 'PASS':
-            all_csv_row.append(csv_row)
 
 elif '.scj' in files[0]:
     for f in files:
@@ -163,85 +155,99 @@ elif '.scj' in files[0]:
             if data['status'] != 'PASS':
                 continue
             print "Using " + files[0] + " to list items"
-            column_num = 5
+            mea_row = []
             for i in range (0, len(data['phases'])):
                 for mea in data['phases'][i]['measurements']:
-                    worksheet_all.write_string(0,column_num, mea['name'])
+                    ITEMs.append(mea['name'])
                     if 'numeric_min' in mea:
-                        worksheet_all.write_number(1,column_num,mea['numeric_min']) # Number Min
+                        LSLs.append(mea['numeric_min'])
+                    else:
+                        LSLs.append('')
                     if 'numeric_max' in mea:
-                        worksheet_all.write_number(2,column_num,mea['numeric_max']) # Number Max
-                    if 'numeric_value' in mea and mea['text_value'] != "inf":
-                        cpk_columns.append(column_num)
-                        csv_items.append(mea['name'])
-                        csv_lsl.append(mea['numeric_min'] if mea.has_key('numeric_min') else '')
-                        csv_usl.append(mea['numeric_max'] if mea.has_key('numeric_max') else '')
-                    column_num += 1
+                        USLs.append(mea['numeric_max'])
+                    else:
+                        USLs.append('')
             break
 
-    all_csv_row = []
+    all_data_rows = []
 
     for f in files:
         with open(f,'r') as json_file:
             data = json.load(json_file)
 
-            worksheet_all.write_string(all_row_num, 0, f) # filename
-            worksheet_all.write_string(all_row_num, 1, data['dut_id']) # DUT_ID
             start_date_time = asctime(gmtime(data['start_time_ms']/1000)) # START_DATE_TIME
-            worksheet_all.write_string(all_row_num, 2, start_date_time)
-            worksheet_all.write_string(all_row_num, 3, data['status']) # STATUS
-            # worksheet_all.write_string(all_row_num, 4, ) # FAILURE_CODE
+            mea_row = [f, data['dut_id'], start_date_time, data['status'], '']
 
-            csv_row = [start_date_time, data['dut_id']]
-
-            column_num = 5
             for i in range (0, len(data['phases'])):
                 for mea in data['phases'][i]['measurements']:
                     if 'numeric_value' in mea and mea['text_value'] != "inf":
-                        worksheet_all.write_number(all_row_num, column_num, mea['numeric_value'])
-                        if mea['name'] in csv_items:
-                            csv_row.append(mea['numeric_value'])
+                        mea_row.append(mea['numeric_value'])
                     else:
-                        worksheet_all.write_string(all_row_num, column_num, mea['text_value'])
-                    column_num += 1
-            if data['status'] == 'PASS':
-                all_csv_row.append(csv_row)
-            all_row_num += 1
+                        mea_row.append(mea['text_value'])
 
-csvwriter.writerow(['DUT_ID'] + csv_items)
-csvwriter.writerow(['LSL'] + csv_lsl)
-csvwriter.writerow(['USL'] + csv_usl)
-for row in sorted(all_csv_row, key = operator.itemgetter(0), reverse=False):
-    csvwriter.writerow(row[1:])
+            all_data_rows.append(mea_row)
 
-for i in cpk_columns:
+mea_data_sorted = sorted(all_data_rows, key = operator.itemgetter(2), reverse=False) # sort by time
+
+worksheet_all.write_row(0,0,ITEMs)
+worksheet_all.write_row(1,0,LSLs)
+worksheet_all.write_row(2,0,USLs)
+worksheet_all.write_column(3,0,['MIN','MAX','CPK','CPK-','CPK+','STDEV','AVERAGE','Useful Count'])
+
+for i in range(0, len(mea_data_sorted)):
+    worksheet_all.write_row(11+i, 0, mea_data_sorted[i])
+
+worksheet_all.freeze_panes(11,1)
+
+numeric_columns = []
+for i in range(0, len(mea_data_sorted)):
+    # find the pass one
+    row = mea_data_sorted[i]
+    if row[3] == 'PASS':
+        for j in range(5, len(row)):
+            if type(row[j]) in (int, float):
+                numeric_columns.append(j)
+        break
+
+all_row_num = 11 + len(mea_data_sorted)
+for i in numeric_columns:
     column_letter = xl_col_to_name(i)
     worksheet_all.write_array_formula(3,i,3,i,'=MIN(IF($D$12:$D${0}="PASS",{1},""))'.format(all_row_num,xl_range(11,i,all_row_num-1,i)))
     worksheet_all.write_array_formula(4,i,4,i,'=MAX(IF($D$12:$D${0}="PASS",{1},""))'.format(all_row_num,xl_range(11,i,all_row_num-1,i)))
-    worksheet_all.write_array_formula(5,i,5,i,'=MIN({0}7:{0}8)'.format(column_letter))
-    worksheet_all.write_array_formula(6,i,6,i,'=ABS({0}10-{0}2)/(3*{0}9)'.format(column_letter))
-    worksheet_all.write_array_formula(7,i,7,i,'=ABS({0}3-{0}10)/(3*{0}9)'.format(column_letter))
+    if LSLs[i] != '':
+        worksheet_all.write_array_formula(6,i,6,i,'=ABS({0}10-{0}2)/(3*{0}9)'.format(column_letter)) # CPK-
+        set_conditional_format(worksheet_all, 6, i)
+    if USLs[i] != '':
+        worksheet_all.write_array_formula(7,i,7,i,'=ABS({0}3-{0}10)/(3*{0}9)'.format(column_letter)) # CPK+
+        set_conditional_format(worksheet_all, 7, i)
+    if LSLs[i] != '' or USLs[i] != '':
+        worksheet_all.write_array_formula(5,i,5,i,'=MIN({0}7:{0}8)'.format(column_letter)) # CPK
+        set_conditional_format(worksheet_all, 5, i)
     worksheet_all.write_array_formula(8,i,8,i,'=STDEV(IF($D$12:$D${0}="PASS",{1},""))'.format(all_row_num,xl_range(11,i,all_row_num-1,i)))
     worksheet_all.write_array_formula(9,i,9,i,'=AVERAGE(IF($D$12:$D${0}="PASS",{1},""))'.format(all_row_num,xl_range(11,i,all_row_num-1,i)))
     worksheet_all.write_array_formula(10,i,10,i,'=COUNT(IF($D$12:$D${0}="PASS",{1},""))'.format(all_row_num,xl_range(11,i,all_row_num-1,i)))
 
-    worksheet_all.conditional_format(5,i,7,i,{'type': 'cell',
-                                                       'criteria': '>=',
-                                                       'value': 2,
-                                                       'format': format_green})
-    worksheet_all.conditional_format(5,i,7,i,{'type': 'cell',
-                                                       'criteria': 'between',
-                                                       'minimum': 1,
-                                                       'maximum': 1.33,
-                                                       'format': format_yellow})
-    worksheet_all.conditional_format(5,i,7,i,{'type': 'cell',
-                                                       'criteria': 'between',
-                                                       'minimum': 0.67,
-                                                       'maximum': 1,
-                                                       'format': format_pink})
-    worksheet_all.conditional_format(5,i,7,i,{'type': 'cell',
-                                                       'criteria': '<',
-                                                       'value': 0.67,
-                                                       'format': format_red})
+csv_items = ['DUT_ID']
+csv_lsl = ['LSL']
+csv_usl = ['USL']
+for i in range(1,len(ITEMs)):
+    # if i in numeric_columns:
+    if LSLs[i] != '' or USLs[i] != '':
+        csv_items.append(ITEMs[i])
+        csv_lsl.append(LSLs[i])
+        csv_usl.append(USLs[i])
+
+csvwriter.writerows([csv_items,csv_lsl,csv_usl])
+
+for data in mea_data_sorted:
+    row = []
+    if data[3] == 'PASS':
+        row.append(data[1])
+        # for i in numeric_columns:
+        for i in range(1, len(LSLs)):
+            if LSLs[i] != '' or USLs[i] != '':
+                row.append(data[i])
+        csvwriter.writerow(row)
+
 workbook.close()
 cpk_csv.close()
